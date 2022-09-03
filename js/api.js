e01e489bff5709d515432d58a270ecbd69e6ff4f
aroy15 <anjonroy57@gmail.com>
2022-09-03 18:33:41 +0600

    full card dynamic and content slice after 350 text length

--- js/api.js
+++ js/api.js
@@ -27,7 +27,8 @@ const displayNews = (data)=>{
     const showNews = getId('displayNews');
     showNews.innerHTML='';
     data.forEach(news => {
-        const {image_url, thumbnail_url, title, toral_view, category_id} = news;
+        const {image_url, thumbnail_url, title, total_view, category_id, details} = news;
+        const {img, name, published_date} = news.author;
         const newsItem = document.createElement('div');
         newsItem.classList.add('news_item');
         newsItem.innerHTML = `
@@ -38,30 +39,29 @@ const displayNews = (data)=>{
                 </div>
                 <!-- post content -->
                 <div class="p-3">
-                    <h2 class="text-dark mb-3">The best fashion influencers to follow for sartorial inspiration</h2>
-                    <div id="postContent">
-                        <p>From our favourite UK influencers to the best missives from Milan and the coolest New Yorkers, read on some of the best fashion blogs out there, and for even more inspiration, do head to our separate black fashion influencer round-up.</p>
-                        <p>Fancy some shopping deals? Check out these amazing sales: Zara Black Friday, ASOS Black Friday, Missoma Black Friday and Gucci Black Friday...</p>
+                    <h2 class="text-dark mb-3">${title}</h2>
+                    <div class="post_content">
+                        <p>${details.length > 350 ? details.slice(0,350)+'...' : details}</p>
                     </div>
                     <!-- post info -->
                     <div class="d-flex justify-content-between gap-4 mt-4 align-items-center">
                         <!-- author info -->
                         <div class="d-flex align-items-center flex-column flex-sm-row gap-2">
                             <div class="author">
-                                <img src="img/author.png" alt="">
+                                <img src="${img}" alt="${name}">
                             </div>
                             <div>
-                                <p class="mb-0 text-dark">Jane Cooper</p>
-                                <p class="mb-0">Jan 10,2022</p>
+                                <p class="mb-0 text-dark">${name ? name : 'no name'}</p>
+                                <p class="mb-0">${published_date}</p>
                             </div>
                         </div>
                         <!-- post view -->                                        
                         <div class="d-flex align-items-center gap-2 text-dark">
                             <i class="fa-regular fa-eye"></i>
-                            <span><strong>1.5M</strong></span>
+                            <span><strong>${total_view?total_view:'0'}M</strong></span>
                         </div>
                             <!-- Button trigger modal -->
-                        <a class="pointer text-primary h3 cursor-pointer" data-bs-toggle="modal" data-bs-target="#detailNewsModal">
+                        <a class="pointer text-primary h3 cursor-pointer" onclick="newsDetials('${category_id}')" data-bs-toggle="modal" data-bs-target="#detailNewsModal">
                             <i class="fa-solid fa-arrow-right"></i>
                         </a>
                     </div>
