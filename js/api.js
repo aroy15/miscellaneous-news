2d165a687083f7b3cd1f3a99787461776d216b89
aroy15 <anjonroy57@gmail.com>
2022-09-03 17:33:19 +0600

    category menu added using api

--- js/api.js
+++ js/api.js
@@ -1,1 +1,17 @@
-console.log('api js connected')
+const getId = id => document.getElementById(id);
+
+const loadCategory = ()=>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategory(data.data))
+    .catch(error => console.log(error));
+}
+const displayCategory = (data)=>{
+    const showCategroy = getId('displayCategory');
+    data.news_category.forEach(item => {
+        const {category_id, category_name} = item;
+        showCategroy.innerHTML += `<a class="nav-link pointer" onclick="displayNews('cateId${category_id}')"  data-bs-toggle="pill">${category_name}</a>`;
+    });
+}
+loadCategory();
